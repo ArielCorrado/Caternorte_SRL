c1e27d06355078599f946bc7420c00876eceb8c2
Ariel Corrado <jarielc2012@yahoo.com.ar>
2023-11-07 11:15:43 -0300

    fotos en calidad

--- src/pages/quality/Quality.tsx
+++ src/pages/quality/Quality.tsx
@@ -34,21 +34,48 @@ function Quality() {
                         Medición de metales
                     </p>
                     <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
-                        <img src="/images/quality/durometro.jpg" alt="Durometro" className="qualityCardImg" />
+                        <img src="/images/quality/microscopio.jpg" alt="Durometro" className="qualityCardImg" />
                         <div className="qualityCardTitleCont flex">
-                            <p className="qualityCardTitle">Durometro ROCKWELL</p> 
+                            <p className="qualityCardTitle">Microscopio</p>
                         </div>
                         <div className="qualityCardDescCont flex">
-                            <p className="qualityCardDesc">Durometro ROCKWELL B y C: equipo para determinar durezas en dichas escalas de los aceros.</p>
+                            <p className="qualityCardDesc">Microscopio metalúrgico: con el cual podemos determinar la estructura y el grafito obtenido en nuestra planta.</p>
                         </div>
                     </div>
                     <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
-                        <img src="/images/quality/pirometro.jpg" alt="Durometro" className="qualityCardImg" />
+                        <img src="/images/quality/durometro.jpg" alt="Durometro" className="qualityCardImg" />
                         <div className="qualityCardTitleCont flex">
-                            <p className="qualityCardTitle">Pirómetro de inmersión</p>
+                            <p className="qualityCardTitle">Durómetro</p>
                         </div>
                         <div className="qualityCardDescCont flex">
-                            <p className="qualityCardDesc">Equipo para determinar las temperaturas tanto de hornos como de coladas de las piezas</p>
+                            <p className="qualityCardDesc">Durómetro de pie BRINELL: con el cual determinamos la dureza de los materiales producidos.</p>
+                        </div>
+                    </div>
+                    <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
+                        <img src="/images/quality/cortadora.jpg" alt="Durometro" className="qualityCardImg" />
+                        <div className="qualityCardTitleCont flex">
+                            <p className="qualityCardTitle">Cortadora</p>
+                        </div>
+                        <div className="qualityCardDescCont flex">
+                            <p className="qualityCardDesc">Cortadora sensitiva PRAZIS: para cortar las probetas las cuales serán analizadas en el microscopio metalúrgico.</p>
+                        </div>
+                    </div>
+                    <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
+                        <img src="/images/quality/compactador.jpg" alt="Durometro" className="qualityCardImg" />
+                        <div className="qualityCardTitleCont flex">
+                            <p className="qualityCardTitle">Compactador</p>
+                        </div>
+                        <div className="qualityCardDescCont flex">
+                            <p className="qualityCardDesc">Compactador.</p>
+                        </div>
+                    </div>
+                    <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
+                        <img src="/images/quality/pulidora.jpg" alt="Pulidora" className="qualityCardImg" />
+                        <div className="qualityCardTitleCont flex">
+                            <p className="qualityCardTitle">Pulidora</p> 
+                        </div>
+                        <div className="qualityCardDescCont flex">
+                            <p className="qualityCardDesc">Pulidora de paño PRAZIS: esta pulidora se utiliza para realizar las micrografías de los materiales.</p>
                         </div>
                     </div>
                 </div>
@@ -57,12 +84,31 @@ function Quality() {
                         Medición de tierras
                     </p>
                     <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
-                        <img src="/images/quality/phmetro.jpg" alt="Durometro" className="qualityCardImg" />
+                        <img src="/images/quality/balanza.jpg" alt="Durometro" className="qualityCardImg" />
                         <div className="qualityCardTitleCont flex">
-                            <p className="qualityCardTitle">PHmetro</p>
+                            <p className="qualityCardTitle">Balanza</p>
                         </div>
                         <div className="qualityCardDescCont flex">
-                            <p className="qualityCardDesc">Este equipo nos proporciona el pH (acidez) de las tierras de moldeo</p>
+                            <p className="qualityCardDesc">Balanza secadora electrónica: para determinar la humedad de las tierras de moldeo  </p>
+                        </div>
+                    </div>
+                    <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
+                        <img src="/images/quality/resistencia.jpg" alt="Durometro" className="qualityCardImg" />
+                        <div className="qualityCardTitleCont flex">
+                            <p className="qualityCardTitle">Equipo de resistencia</p>
+                        </div>
+                        <div className="qualityCardDescCont flex">
+                            <p className="qualityCardDesc">Equipo de resistencia a la compresión, y partición de las tierras de moldeo que nos van a determinar dichas propiedades, con lo cual podemos garantizar una calidad de terminación satisfactoria.</p>
+                        </div>
+                        
+                    </div>
+                    <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
+                        <img src="/images/quality/tamizadora.jpg" alt="Durometro" className="qualityCardImg" />
+                        <div className="qualityCardTitleCont flex">
+                            <p className="qualityCardTitle">Equipo de tamices</p>
+                        </div>
+                        <div className="qualityCardDescCont flex">
+                            <p className="qualityCardDesc">Equipo de tamices para análisis granulométrico: utilizado para determinar la granulometría de las arenas de moldeo.</p>
                         </div>
                     </div>
                     <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
@@ -71,7 +117,16 @@ function Quality() {
                             <p className="qualityCardTitle">Permeámetro</p>
                         </div>
                         <div className="qualityCardDescCont flex">
-                            <p className="qualityCardDesc">Nos proporciona la capacidad de evacuar gases en las tierras de moldeo.</p>
+                            <p className="qualityCardDesc">Permeámetro: que nos proporciona la capacidad de evacuar gases en las tierras de moldeo.</p>
+                        </div>
+                    </div>
+                    <div className="qualityCardCont OoS" onMouseOver={(e) => showDescription(e)} onMouseLeave={(e) => hiddenDescription(e)}>
+                        <img src="/images/quality/tolva.jpg" alt="Durometro" className="qualityCardImg" />
+                        <div className="qualityCardTitleCont flex">
+                            <p className="qualityCardTitle">Tolva de carga</p>
+                        </div>
+                        <div className="qualityCardDescCont flex">
+                            <p className="qualityCardDesc">Tolva de carga.</p>
                         </div>
                     </div>
                 </div>
