e0ff363113f677145174d22c1d0f0164f2aa27ee
Ariel Corrado <jarielc2012@yahoo.com.ar>
2023-11-07 14:14:25 -0300

    cm

--- src/pages/quality/Quality.tsx
+++ src/pages/quality/Quality.tsx
@@ -15,7 +15,7 @@ function Quality() {
     return (
         <div className="seccions qualitySeccion flex column">
 
-            <img src="/images/quality/bgmin.jpg" alt="Caternorte calidad background" className="qualityBg OoS"/>
+            <img src="/images/backgrounds/quality.jpg" alt="Caternorte calidad background" className="qualityBg OoS"/>
             <div className="qualityBgFilter"></div>
           
             <div className="qualityIntroCont flex column OoS">
