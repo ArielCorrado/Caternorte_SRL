02c686e98d8263ca05312d409f22f4a38cf1583c
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-02-26 22:19:01 -0300

    subiendo web a hostinger

--- public/sendmail.php
+++ public/sendmail.php
@@ -2,7 +2,7 @@
     $jsonData = file_get_contents('php://input');
     $data = json_decode($jsonData, true);
 
-    $to = 'info@caternortesrl.com.ar, ariel.corrado27@gmail.com';
+    $to = 'info@caternortesrl.com.ar';
     $from = 'info@caternortesrl.com.ar';
         
     $nombre = utf8_decode($data['nombre']);
