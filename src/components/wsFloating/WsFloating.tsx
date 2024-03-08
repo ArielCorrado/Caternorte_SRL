a3563fd0fb0484b9cd20334619b652295c465973
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-03-08 14:08:46 -0300

    se agregó envío de conversión al hacer click en whatsapp flotante

--- src/components/wsFloating/WsFloating.tsx
+++ src/components/wsFloating/WsFloating.tsx
@@ -1,8 +1,11 @@
 import "./wsFloating.css";
+import { gtag_report_conversion } from "../../ads/contact";
 
 function WsFloating() {
     return (
-        <a href="https://wa.me/+5491134713398" target="_blank" rel="noreferrer" className="wsFloatingCont OoS"><img src="/images/icons/wsfloating.png" alt="Caternorte Whatsapp Floating" className="wsFloating"/></a>
+        <a href="https://wa.me/+5491134713398" target="_blank" rel="noreferrer" className="wsFloatingCont OoS" onClick={gtag_report_conversion}>
+            <img src="/images/icons/wsfloating.png" alt="Caternorte Whatsapp Floating" className="wsFloating"/>
+        </a>
     )
 }
 
